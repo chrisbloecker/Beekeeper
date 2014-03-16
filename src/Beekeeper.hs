8a944857fdaa3944f8f4fd4e27e4037c35ad6601
Christopher Blöcker <chrisbloecker@googlemail.com>
2014-03-16 18:36:29 +0100

    Names changed

--- src/Beekeeper.hs
+++ src/Beekeeper.hs
@@ -24,17 +24,29 @@ main = do
   let rt = remoteTable initRemoteTable
   args <- getArgs
   case args of
+    ("help":_) -> do
+      putStrLn "Usage:"
+      putStrLn ""
+      putStrLn "'queen' host port"
+      putStrLn "  to start a queen on host using port"
+      putStrLn ""
+      putStrLn "'drone' host port"
+      putStrLn "  to start a drone on host using port"
+      putStrLn ""
+      putStrLn "'client host port problem"
+      putStrLn "  to start a client on host using port, requestion to solve Problem"
+
     ["queen", host, port] -> do
       context <- initializeBackend host port rt
       node    <- newLocalNode context
 
-      runProcess node $ startQueen context
+      runProcess node $ runQueen context
 
     ["drone", host, port] -> do
       context <- initializeBackend host port rt
       node    <- newLocalNode context
 
-      runProcess node $ startDrone context
+      runProcess node $ runDrone context
     
     ["client", host, port, problem] -> do
       context <- initializeBackend host port rt
