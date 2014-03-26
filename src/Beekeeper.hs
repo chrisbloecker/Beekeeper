b6166259a68bab7a1152bf847fa1c1ab5ed1c30d
Christopher Blöcker <chrisbloecker@googlemail.com>
2014-03-26 13:51:33 +0100

    no simple-localnet, using Hive 0.1.1.0

--- src/Beekeeper.hs
+++ src/Beekeeper.hs
@@ -3,17 +3,16 @@ module Main
 
 import System.Environment (getArgs)
 
-import Control.Concurrent.MVar
---import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Concurrent.MVar (newEmptyMVar, takeMVar)
 import Control.Distributed.Process.Node
 import Network.Transport.TCP
 
 import Data.Time      (getCurrentTime, diffUTCTime)
 import Data.Text.Lazy (pack, unpack)
 
-import Hive.Queen
-import Hive.Drone
-import Hive.Client
+import Hive.Queen  (runQueen)
+import Hive.Drone  (runDrone)
+import Hive.Client (solveRequest)
 import Hive.Types
 
 import Hive.RemoteTable (remoteTable)
@@ -22,59 +21,60 @@ import qualified Data.Text.Lazy.IO as IOText (readFile)
 
 -------------------------------------------------------------------------------
 
+mkNode :: String -> String -> IO LocalNode
+mkNode host port = do
+  Right transport <- createTransport host port defaultTCPParameters
+  newLocalNode transport (remoteTable initRemoteTable) -- ToDo: will this cause problems?
+
+help :: IO ()
+help = do
+  putStrLn "Usage:"
+  putStrLn ""
+  putStrLn "'queen' host port"
+  putStrLn "  to start a queen on host using port"
+  putStrLn ""
+  putStrLn "'drone' host port queenHost queenPort"
+  putStrLn "  to start a drone on host using port"
+  putStrLn ""
+  putStrLn "'client host port problem"
+  putStrLn "  to start a client on host using port, requestion to solve Problem"
+
+handleSolution :: Solution -> IO ()
+handleSolution (Solution _ _)  = return ()
+handleSolution  NoSolution     = putStrLn "Sorry, we couldn't find a solution."
+handleSolution  InvalidInput   = putStrLn "Your input is invalid."
+handleSolution  TimeoutReached = putStrLn "Timeout Reached."
+handleSolution  NotImplemented = putStrLn "This is not yet implemented."
+
 main :: IO ()
 main = do
-  let rt = remoteTable initRemoteTable
   args <- getArgs
   case args of
-    ("help":_) -> do
-      putStrLn "Usage:"
-      putStrLn ""
-      putStrLn "'queen' host port"
-      putStrLn "  to start a queen on host using port"
-      putStrLn ""
-      putStrLn "'drone' host port queenAddress"
-      putStrLn "  to start a drone on host using port"
-      putStrLn ""
-      putStrLn "'client host port problem"
-      putStrLn "  to start a client on host using port, requestion to solve Problem"
+    ("help":_) -> help
 
     ["queen", host, port] -> do
-      Right transport <- createTransport host port defaultTCPParameters
-      node    <- newLocalNode transport rt
-
+      node <- mkNode host port
       runProcess node runQueen
 
-    ["drone", host, port, queenAddress] -> do
-      Right transport <- createTransport host port defaultTCPParameters
-      node    <- newLocalNode transport rt
-
-      runProcess node $ runDrone queenAddress
+    ["drone", host, port, queenHost, queenPort] -> do
+      node <- mkNode host port
+      runProcess node $ runDrone queenHost queenPort
     
-    ["client", host, port, problem, queenAddress] -> do
-      Right transport <- createTransport host port defaultTCPParameters
-      node    <- newLocalNode transport rt
-      mvar    <- newEmptyMVar
-
-      runProcess node $ solveRequest queenAddress (Problem TSP (Instance $ pack problem)) mvar (minutes 1)
+    ["client", host, port, problem, queenHost, queenPort] -> do
+      node <- mkNode host port
+      mvar <- newEmptyMVar
+      runProcess node $ solveRequest queenHost queenPort (Problem TSP (Instance $ pack problem)) mvar (minutes 1)
       putStrLn . unpack . unSolution =<< takeMVar mvar
 
-    ["cli", host, port, queenAddress, filepath] -> do
-      Right transport <- createTransport host port defaultTCPParameters
-      node    <- newLocalNode transport rt
-      mvar    <- newEmptyMVar
+    ["cli", host, port, queenHost, queenPort, filepath] -> do
+      node <- mkNode host port
+      mvar <- newEmptyMVar
 
       fileContent <- IOText.readFile filepath
-      start <- getCurrentTime
-      runProcess node $ solveRequest queenAddress (Problem SSSP (Instance fileContent)) mvar (minutes 2)
-      solution <- takeMVar mvar
-      end <- getCurrentTime
-      case solution of
-        Solution _ _   -> return ()
-        NoSolution     -> putStrLn "Sorry, we couldn't find a solution."
-        InvalidInput   -> putStrLn "Your input is invalid."
-        TimeoutReached -> putStrLn "Timeout Reached."
-        NotImplemented -> putStrLn "This is not yet implemented."
+      start       <- getCurrentTime
+      runProcess node $ solveRequest queenHost queenPort (Problem SSSP (Instance fileContent)) mvar (minutes 2)
+      handleSolution =<< takeMVar mvar
+      end         <- getCurrentTime
 
       putStrLn $ "This took " ++ show (diffUTCTime end start)
       
--- startDrones.sh
+++ startDrones.sh
@@ -4,15 +4,20 @@ help()
 {
 echo "Usage: startDrones.sh <opts>
 Opts are those:
-    -h,  --help              : Print this message and exit
-    -d,  --drones <number>   : How many drones should be started
-    -p,  --port <number>     : Port of the first drone"
+    -h,  --help               : Print this message and exit
+    -d,  --drones <number>    : How many drones should be started
+    -ip  --ip                 : Host of the drones
+    -p,  --port <number>      : Port of the first drone (next drones use subsequent ports)
+    -qip --queenip <number>   : Host of the queen
+    -qp  --queenport <number> : Port of the queen"
 exit $1
 }
 
 drones=$(nproc)
 port=50000
 ip=$(hostname -I)
+qip=$(hostname -I)
+qp=42000
 
 while [ $# -gt 1 ]; do
   case $1 in
@@ -20,13 +25,21 @@ while [ $# -gt 1 ]; do
       shift
       drones=$1
     ;;
-    --ip)
+    -ip | --ip)
       shift
       ip=$1
     ;;
     -p | --port)
       shift
       port=$1
+    ;;
+    -qip | --queenip)
+      shift
+      qip=$1
+    ;;
+    -qp | --queenport)
+      shift
+      qp=$1
     ;;
     -h | --help)
       help 0
@@ -40,6 +53,6 @@ done
 
 for i in $(seq 1 $drones); do
   echo "starting drone "${i}"..."
-  ./dist/build/Beekeeper/Beekeeper "drone" $ip $port &
+  ./dist/build/Beekeeper/Beekeeper "drone" $ip $port $qip $qp &
   port=$(($port+1))
 done
